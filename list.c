d468216516ef4ccfeb2974069239bc0eb34b3215
arunench <arunench@gmail.com>
2015-03-23 21:14:55 -0400

    Updated

--- list.c
+++ list.c
@@ -1,149 +1,77 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
- 
-struct node
-{
-    int data;
-    struct node *next;
-}*head;
- 
- 
- 
-void append(int num)
-{
-    struct node *temp,*right;
-    temp= (struct node *)malloc(sizeof(struct node));
-    temp->data=num;
-    right=(struct node *)head;
-    while(right->next != NULL)
-    right=right->next;
-    right->next =temp;
-    right=temp;
-    right->next=NULL;
-}
- 
- 
- 
-void add( int num )
-{
-    struct node *temp;
-    temp=(struct node *)malloc(sizeof(struct node));
-    temp->data=num;
-    if (head== NULL)
-    {
-    head=temp;
-    head->next=NULL;
-    }
-    else
-    {
-    temp->next=head;
-    head=temp;
-    }
-}
-void addafter(int num, int loc)
-{
-    int i;
-    struct node *temp,*left,*right;
-    right=head;
-    for(i=1;i<loc;i++)
-    {
-    left=right;
-    right=right->next;
-    }
-    temp=(struct node *)malloc(sizeof(struct node));
-    temp->data=num;
-    left->next=temp;
-    left=temp;
-    left->next=right;
-    return;
-}
- 
- 
- 
-void insert(int num)
-{
-    int c=0;
-    struct node *temp;
-    temp=head;
-    if(temp==NULL)
-    {
-    add(num);
-    }
-    else
-    {
-    while(temp!=NULL)
-    {
-        if(temp->data<num)
-        c++;
-        temp=temp->next;
-    }
-    if(c==0)
-        add(num);
-    else if(c<count())
-        addafter(num,++c);
-    else
-        append(num);
-    }
-}
- 
- 
- 
-int delete(int num)
-{
-    struct node *temp, *prev;
-    temp=head;
-    while(temp!=NULL)
-    {
-    if(temp->data==num)
-    {
-        if(temp==head)
-        {
-        head=temp->next;
-        free(temp);
-        return 1;
+#include "list.h"
+
+
+struct node *find(int number){		
+
+        if (first->head == number)
+        {		
+            // If the value we want to delete is the first number, set the first to the next
+                point = first->tail;	
+                return first;		
+        }
+        while (first->tail->head != number && first->tail->tail != NULL)	
+             { 
+                first = first->tail;					
+                }
+        if (first->tail->head != number)
+        {		
+                return NULL;			
         }
         else
         {
-        prev->next=temp->next;
-        free(temp);
-        return 1;
+                // If the desired number is in the list, the node with that value will be returned
+                return first->tail;	
         }
-    }
-    else
-    {
-        prev=temp;
-        temp= temp->next;
-    }
-    }
-    return 0;
 }
- 
- 
-void  prettyPrint(struct node *r)
+
+int delete(int number)
+        {
+
+        //Create a temporary node
+        struct node *temp; 
+        // If the number was not in the list   
+        if (find(number) == NULL)
+        {  
+            
+                first = point;  
+                printf("Your number is not in the list.\n");
+
+        return 0;   
+        }
+        else
+        {
+                temp = find(number);  //find temp node with value of number
+                first->tail = temp->tail; //attach first->tail to the next tail
+                first = point;  //reset iterator
+                printf("Your number was deleted successufly.\n");
+                return 1;
+        }
+
+
+
+
+
+void add(int number)
 {
-    r=head;
-    if(r==NULL)
-    {
-    return;
+
+    struct node *newNode;
+    struct node *current;
+    newNode = (struct node*)malloc(sizeof(struct node));    
+    if(newNode == NULL){
+        fprintf(stderr, "Error: cannot add another node\n");    
+            exit(-1);
     }
-    while(r!=NULL)
-    {
-    printf("%d ",r->data);
-    r=r->next;
-    }
-    printf("\n");
+    newNode->head = number; 
+    current = (struct node*) first; 
+    while (current->tail != NULL)   
+       { 
+        current = current->tail;
+        }
+    current->tail = newNode;    
+    current = newNode;      
+    current->tail = NULL;       
 }
- 
- 
-int count()
-{
-    struct node *n;
-    int c=0;
-    n=head;
-    while(n!=NULL)
-    {
-    n=n->next;
-    c++;
-    }
-    return c;
 }
